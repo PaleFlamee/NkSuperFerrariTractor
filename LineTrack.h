97f0ae77b1efc891f7df29a735b39146a6fffcbc
PaleFlame <drminghaochen@gmail.com>
2025-11-23 00:46:42 +0800

    update

---
+++ LineTrack.h
@@ -0,0 +1,13 @@
+#ifndef	__LINE_TRACK_H__
+#define __LINE_TRACK_H__
+
+#include "ti_msp_dl_config.h"
+// 这TM是啥玩意儿，德州仪器的GPIO读引脚函数吗？
+
+int xunji(void);
+int xunji_highspeed_left(void);
+int xunji_highspeed_right(void);
+int xunji_lowspeed_left(void);
+int xunji_lowspeed_right(void);
+
+#endif
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 看看README啊啊啊
 ## @wawa
-### 你那个xunji要改，没那么多GPIO给你读，把LTMData给你放参数里面
+### 你那个xunji要改，没那么多GPIO给你读，把LTMData给你放参数里面，或者全局变量，取决于xunji怎么调用
