5a2534a768e00415463bd999f19903c82888fd18
wyqzx66 <1961085291@qq.com>
2025-11-22 22:53:53 +0800

    Add files via upload

---
+++ xunji.h
@@ -0,0 +1,12 @@
+#ifndef	__XUNJI_H__
+#define __XUNJI_H__
+
+#include "ti_msp_dl_config.h"
+
+int xunji(void);
+int xunji_highspeed_left(void);
+int xunji_highspeed_right(void);
+int xunji_lowspeed_left(void);
+int xunji_lowspeed_right(void);
+
+#endif
