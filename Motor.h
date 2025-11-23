b3b02640d61ce1a593dd09dbd3b87f456da11576
PaleFlame <drminghaochen@gmail.com>
2025-11-23 16:44:52 +0800

    .

--- Motor.h
+++ Motor.h
@@ -2,14 +2,14 @@
 #define MOTOR_H
 
 // Pin definitions
-#define PWMA 3  //3为模拟引脚，用于PWM控制
+#define PWMA 3
 #define AIN1 5
 #define AIN2 4
-#define PWMB 10  //10为模拟引脚，用于PWM控制
+#define PWMB 10
 #define BIN1 8
 #define BIN2 9
-#define STBY 7  //2、4、8、12、7为数字引脚，用于开关量控制
-#define Voltage A0 //使用模拟引脚
+#define STBY 7
+#define Voltage A0 // Battery voltage reading pin, analog input
 
 // Motor identifiers
 #define MOTOR_LEFT 1  // Motor A
